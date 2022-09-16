0632f579fc602b29880630dd8cc819dd41965fe2
Keydi <keydi.garcia@vtsd.com>
2022-09-16 10:40:25 -0400

    Lab Finished

---
+++ Lab02.java
@@ -0,0 +1,27 @@
+public class Lab02 {
+    public static void main(String args[])
+
+    {
+        System.out.println("Lab 02, 80 point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge");
+        System.out.println("by yesterday's confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+
+    System.out.println("# ##### #####\t##### ##### # #   # ##### #####");
+    System.out.println("# #   # #   #\t#   # #   # # ##  #   #   #");
+    System.out.println("# #   # #   #\t##### #   # # # # #   #   #####");
+    System.out.println("# #   # #   #\t#     #   # # #  ##   #       #");
+    System.out.println("# ##### #####\t#     ##### # #   #   #   #####");
+
+
+
+    }
+
+
+
+
+}
